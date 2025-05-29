43f1f1c503d5db31dc0a08185ec8d620eb626574
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-29 11:20:14 -0300

    default

--- flake.nix
+++ flake.nix
@@ -7,28 +7,38 @@
     };
 
     outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let
-                pkgs = import nixpkgs {
-                    inherit system;
-                    config.allowUnfree = true;
-                };
-
-                customPackages = {
-                    sfmono-liga = pkgs.callPackage ./pkgs/fonts/sfmono_liga.nix {};
-                    blexmono-liga = pkgs.callPackage ./pkgs/fonts/blexmono_liga.nix {};
-                    corkit = pkgs.callPackage ./dev/corkit.nix {};
-                    lightorch = pkgs.callPackage ./dev/lightorch.nix {};
-                    nvidia_nemo = pkgs.callPackage ./dev/nemo.nix {};
-                    nvidia_physicsnemo = pkgs.callPackage ./dev/physicsnemo.nix {};
-                    starstream = pkgs.callPackage ./dev/starstream.nix {};
-                };
-            in {
-                packages = customPackages;
-                defaultPackage = customPackages.lightorch;
-                overlays.default = final: prev: customPackages;
-            }
-        ) // {
-            overlay = self.overlays.default;
+        let
+            overlay = final: prev: {
+                sfmono-liga = final.callPackage ./pkgs/fonts/sfmono_liga.nix {};
+                blexmono-liga = final.callPackage ./pkgs/fonts/blexmono_liga.nix {};
+                corkit = final.callPackage ./dev/corkit.nix {};
+                lightorch = final.callPackage ./dev/lightorch.nix {};
+                nvidia_nemo = final.callPackage ./dev/nemo.nix {};
+                nvidia_physicsnemo = final.callPackage ./dev/physicsnemo.nix {};
+                starstream = final.callPackage ./dev/starstream.nix {};
+            };
+        in
+            flake-utils.lib.eachDefaultSystem (system:
+                let
+                    pkgs = import nixpkgs {
+                        inherit system;
+                        overlays = [ overlay ];
+                        config.allowUnfree = true;
+                    };
+                in {
+                    packages = {
+                        inherit (pkgs)
+                        sfmono-liga
+                        blexmono-liga
+                        corkit
+                        lightorch
+                        nvidia_nemo
+                        nvidia_physicsnemo
+                        starstream;
+                    };
+                }
+            ) // {
+            overlays.default = overlay;
         };
 }
+
