154e2cc4b5e65b0c00012abfedd39fc66bdd0ddc
Jorgedavyd <jorged.encyso@gmail.com>
2025-06-05 15:16:08 -0300

    default

--- dev/nemo_guardrails.nix
+++ dev/nemo_guardrails.nix
@@ -1,13 +1,40 @@
-{ py-pkgs, lib }:
+{ lib, py-pkgs, gcc }:
 
 py-pkgs.buildPythonPackage rec {
-    pname = "nemo_guardrails";
-    version = "0.14.0";
-    format = "wheel";
-    src = py-pkgs.fetchPypi {
-        inherit pname version format;
-        sha256 = lib.fakeSha256;
-        dist = "python";
-        python = "py3";
-    };
+  pname = "nemo-guardrails";
+  version = "0.14.0";
+  format = "pyproject";
+
+  src = py-pkgs.fetchFromGitHub {
+    owner = "NVIDIA";
+    repo = "NeMo-Guardrails";
+    rev = "v${version}";
+    hash = "sha256-HcQz7YJZwZ3w8z0nq5xJ7z5q5z5q5z5q5z5q5z5q5z5=";
+  };
+
+  nativeBuildInputs = [
+    gcc
+    py-pkgs.setuptools
+  ];
+
+  propagatedBuildInputs = with py-pkgs; [
+    langchain
+    pydantic
+    requests
+    aiohttp
+    click
+    typing-extensions
+    pyyaml
+    annoy
+  ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "NeMo Guardrails, an open-source toolkit for adding programmable guardrails to LLM-based conversational systems";
+    homepage = "https://github.com/NVIDIA/NeMo-Guardrails";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.all;
+  };
 }
