82677e03286458357ea207c4d5077e804cf95608
Jorgedavyd <jorged.encyso@gmail.com>
2025-06-16 15:48:31 -0300

    added langgraph-checkpoint-postgres

---
+++ dev/langgraph-checkpoint-postgres.nix
@@ -0,0 +1,20 @@
+{ lib, py-pkgs, fetchPypi }:
+
+py-pkgs.buildPythonPackage rec {
+    pname = "langgraph-checkpoint-postgres";
+    version = "2.0.21";
+    src = fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-dGqOXwHfXEQQraav+i/LhbeCKqH2Eg9TWZh8mHDjaxo=";
+    };
+    doCheck = false;
+
+    meta = with lib; {
+        description = "The langgraph-checkpoint-postgres package contains Postgres integrations for memory retrieval";
+        homepage = "https://pypi.org/project/langgraph-checkpoint-postgres/";
+        license = licenses.mit;
+        maintainers = with maintainers; [ ];
+        platforms = platforms.all;
+    };
+}
+
