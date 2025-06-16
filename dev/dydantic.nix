456326b20dead816c560900f697765cf1e498fbd
Jorgedavyd <jorged.encyso@gmail.com>
2025-06-16 17:28:20 -0300

    default

--- dev/dydantic.nix
+++ dev/dydantic.nix
@@ -1,19 +1,23 @@
 { pkgs, poetry2nix }:
+let
+    inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+in
+    mkPoetryApplication {
+        pname = "dydantic";
+        version = "0.0.8";
 
-poetry2nix.mkPoetryApplication {
-  projectDir = pkgs.fetchFromGitHub {
-    owner = "hinthornw";
-    repo = "dydantic";
-    rev = "d35508d5d75a1237e7b6bbd4df838a3dad110408";
-    sha256 = "sha256-iqnBmC4kvypUrKI2D1ud6hnvWe/MX/eyGkxf3phul+4=";
-  };
+        projectDir = pkgs.fetchFromGitHub {
+            owner = "hinthornw";
+            repo = "dydantic";
+            rev = "d35508d5d75a1237e7b6bbd4df838a3dad110408";
+            sha256 = "sha256-iqnBmC4kvypUrKI2D1ud6hnvWe/MX/eyGkxf3phul+4=";
+        };
 
-  doCheck = false;
+        doCheck = false;
 
-  meta = {
-    description = "Dydantic is a Python library for dynamically generating Pydantic models from JSON schemas.";
-    homepage = "https://github.com/hinthornw/dydantic";
-    license = pkgs.lib.licenses.mit;
-  };
-}
-
+        meta = {
+            description = "Dydantic is a Python library for dynamically generating Pydantic models from JSON schemas.";
+            homepage = "https://github.com/hinthornw/dydantic";
+            license = pkgs.lib.licenses.mit;
+        };
+    }
