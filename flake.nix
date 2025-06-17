a0567f7a9956f445aa4f8d5c8223a14f9f67c63f
Jorgedavyd <jorged.encyso@gmail.com>
2025-06-17 11:13:23 -0300

    default

--- flake.nix
+++ flake.nix
@@ -3,15 +3,14 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, flake-utils, ... }:
+    outputs = { self, nixpkgs, ... }:
         let
             overlays = (self: super: {
                 python312 = super.python312.override {
-                    packageOverrides = final: prev: {
-                        mcp = prev.mcp.overridePythonAttrs (old: rec {
+                    packageOverrides = pyself: pysuper: {
+                        mcp = pysuper.mcp.overridePythonAttrs (old: rec {
                             version = "1.9.4";
                             src = self.fetchFromGitHub {
                                 owner = "mcp";
@@ -19,41 +18,28 @@
                                 rev = "v${version}";
                                 sha256 = "sha256-s3Jz16ZXqyUk2yzCUbYna3BUynWZvn0MUZGbQePhRwk=";
                             };
-                            propagatedBuildInputs = old.propagatedBuildInputs or [] ++ [ final.python-multipart ];
+                            dependencies = old.dependencies or [] ++ [ pyself.python-multipart ];
                         });
-                        corkit = final.callPackage ./dev/python/corkit.nix { };
-                        dydantic = final.callPackage ./dev/python/dydantic.nix { };
-                        langchain-mcp-adapters = final.callPackage ./dev/python/langchain-mcp-adapters.nix { };
-                        langchain-milvus = final.callPackage ./dev/python/langchain-milvus.nix { };
-                        langchain-nvidia-ai-endpoints = final.callPackage ./dev/python/langchain-nvidia-ai-endpoints.nix { };
-                        langgraph-swarm = final.callPackage ./dev/python/langgraph-swarm.nix { };
-                        langmem = final.callPackage ./dev/python/langmem.nix { };
-                        lightorch = final.callPackage ./dev/python/lightorch.nix { };
-                        nemo-guardrails = final.callPackage ./dev/python/nemo-guardrails.nix { };
-                        nemo-toolkit = final.callPackage ./dev/python/nemo.nix { };
-                        nvidia-physicsnemo = final.callPackage ./dev/python/physicsnemo.nix { };
-                        starstream = final.callPackage ./dev/python/starstream.nix { };
-                        trustcall = final.callPackage ./dev/python/trustcall.nix { };
+                        corkit = pyself.callPackage ./dev/python/corkit.nix { };
+                        dydantic = pyself.callPackage ./dev/python/dydantic.nix { };
+                        langchain-mcp-adapters = pyself.callPackage ./dev/python/langchain-mcp-adapters.nix { };
+                        langchain-milvus = pyself.callPackage ./dev/python/langchain-milvus.nix { };
+                        langchain-nvidia-ai-endpoints = pyself.callPackage ./dev/python/langchain-nvidia-ai-endpoints.nix { };
+                        langgraph-swarm = pyself.callPackage ./dev/python/langgraph-swarm.nix { };
+                        langmem = pyself.callPackage ./dev/python/langmem.nix { };
+                        lightorch = pyself.callPackage ./dev/python/lightorch.nix { };
+                        nemo-guardrails = pyself.callPackage ./dev/python/nemo-guardrails.nix { };
+                        nemo-toolkit = pyself.callPackage ./dev/python/nemo.nix { };
+                        nvidia-physicsnemo = pyself.callPackage ./dev/python/physicsnemo.nix { };
+                        starstream = pyself.callPackage ./dev/python/starstream.nix { };
+                        trustcall = pyself.callPackage ./dev/python/trustcall.nix { };
                     };
                 };
                 sfmono-liga = super.callPackage ./pkgs/fonts/sfmono-liga.nix { };
                 blexmono-liga = super.callPackage ./pkgs/fonts/blexmono-liga.nix { };
             });
         in
-            flake-utils.lib.eachDefaultSystem (system:
-                let
-                    pkgs = import nixpkgs {
-                        inherit system;
-                        overlays = [ overlays ];
-                        config.allowUnfree = true;
-                    };
-                in {
-                    packages = {
-                        inherit (pkgs) sfmono-liga blexmono-liga;
-                        inherit (pkgs.python312.pkgs) mcp corkit dydantic langchain-mcp-adapters langchain-milvus langchain-nvidia-ai-endpoints langgraph-swarm langmem lightorch nemo-guardrails nemo-toolkit nvidia-physicsnemo starstream trustcall;
-                    };
-                } // {
-                    overlays.default = overlays;
-                }
-            );
+            {
+                overlays.default = overlays;
+            };
 }
