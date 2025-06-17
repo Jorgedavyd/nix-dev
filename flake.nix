b8406ed1b8669151d291b1b7b3d417fadc779416
Jorgedavyd <jorged.encyso@gmail.com>
2025-06-16 22:15:56 -0300

    default

--- flake.nix
+++ flake.nix
@@ -26,7 +26,6 @@
                     langchain-mcp-adapters = final.callPackage ./dev/langchain-mcp-adapters.nix {py-pkgs = py-pkgs; mcp = mcp; };
                     langchain-milvus = final.callPackage ./dev/langchain-milvus.nix {py-pkgs = py-pkgs; };
                     langchain-nvidia-ai-endpoints = final.callPackage ./dev/langchain-nvidia-ai-endpoints.nix {py-pkgs = py-pkgs; };
-                    langchain-nvidia-trt = final.callPackage ./dev/langchain-nvidia-trt.nix {py-pkgs = py-pkgs; };
                     langgraph-checkpoint = final.callPackage ./dev/langgraph-checkpoint.nix {py-pkgs = py-pkgs; };
                     langgraph-checkpoint-postgres = final.callPackage ./dev/langgraph-checkpoint-postgres.nix {py-pkgs = py-pkgs; };
                     langgraph-swarm = final.callPackage ./dev/langgraph-swarm.nix {py-pkgs = py-pkgs; };
